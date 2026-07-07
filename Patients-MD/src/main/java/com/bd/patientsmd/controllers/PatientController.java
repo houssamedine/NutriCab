81b738fc174d6af6cf13bc7746eb5e5a28b49cc6
houssam-edine <houssamedineelazhari@gmail.com>
2026-07-07 11:11:02 +0100

    ajouter le controller des patients

---
+++ Patients-MD/src/main/java/com/bd/patientsmd/controllers/PatientController.java
@@ -0,0 +1,52 @@
+package com.bd.patientsmd.controllers;
+
+import com.bd.patientsmd.models.dtos.PatientDto;
+import com.bd.patientsmd.models.mappers.CreatePatientRequest;
+import com.bd.patientsmd.services.PatientService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public List<PatientDto> getAllPatients(){
+        return patientService.getAllPatients();
+    }
+
+    @PostMapping
+    public PatientDto createPatient(@Valid @RequestBody CreatePatientRequest patientRequest){
+        return patientService.createPatient(patientRequest);
+    }
+
+    @GetMapping("/{id}")
+    public PatientDto getPatientById(@PathVariable Long id){
+        return patientService.getPatientById(id);
+    }
+
+    @PutMapping("/{id}")
+    public PatientDto updatePatient(@PathVariable Long id,@Valid @RequestBody CreatePatientRequest patientRequest){
+        return patientService.updatePatient(id, patientRequest);
+    }
+
+    @GetMapping("/{id}")
+    public void deletePatient(@PathVariable Long id){
+        patientService.deletePatient(id);
+    }
+
+    @GetMapping("/search")
+    public List<PatientDto> searchPatients(@RequestParam String keyword){
+        return patientService.searhPatient(keyword);
+    }
+
+
+}
