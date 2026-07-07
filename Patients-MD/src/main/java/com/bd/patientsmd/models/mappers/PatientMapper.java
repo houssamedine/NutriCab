47537a9ab9c341df82e464533a3a00dde43cfa01
houssam-edine <houssamedineelazhari@gmail.com>
2026-07-07 10:36:01 +0100

    add mapper patient

--- Patients-MD/src/main/java/com/bd/patientsmd/models/mappers/PatientMapper.java
+++ Patients-MD/src/main/java/com/bd/patientsmd/models/mappers/PatientMapper.java
@@ -1,4 +1,23 @@
 package com.bd.patientsmd.models.mappers;
 
+import com.bd.patientsmd.models.dtos.PatientDto;
+import com.bd.patientsmd.models.entites.Patients;
+
 public class PatientMapper {
+
+    public static PatientDto toDto(Patients patient) {
+        double heightM = patient.getHeightCm() / 100;
+        double bmi = patient.getInitialWeightKg() / (heightM * heightM);
+
+        return new PatientDto(
+                patient.getId(),
+                patient.getFullName(),
+                patient.getBirthDate(),
+                patient.getPhone(),
+                patient.getHeightCm(),
+                patient.getInitialWeightKg(),
+                patient.getObjective(),
+                Math.round(bmi * 100.0) / 100.0
+        );
+    }
 }
